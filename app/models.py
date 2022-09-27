664174a52670e1d51b741f97836d188d8d047dd7
Vladimir <veliseykin2000@gmail.com>
2022-09-27 13:16:56 -0700

    Almost done with back-end

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from enum import unique
-from app import db
+from app import db, ma
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+
 
 
 class User(db.Model):
@@ -10,7 +11,7 @@ class User(db.Model):
     email = db.Column(db.String(25), nullable = False, unique = True)
     password = db.Column(db.String(256), nullable = False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    article = db.relationship('Article', backref = 'author')
+    article = db.relationship('Articles', backref = 'author')
 
 
     def __init__(self, **kwargs):
@@ -27,11 +28,17 @@ class User(db.Model):
         db.session.commit()
 
 
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'username', 'email', 'article', 'data_created')
 
-class Article(db.Model):
+user_chema = UserSchema()
+
+
+class Articles(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(50), nullable = False)
-    body = db.Column(db.Text, nullable = False)
+    body = db.Column(db.Text(), nullable = False)
     image_url = db.Column(db.String(300))
     date_created = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -41,3 +48,11 @@ class Article(db.Model):
         self.title = title
         self.body = body
         self.image_url = image_url
+
+class ArticlesSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'body', 'image_url', 'date_created', 'user_id')
+
+article_chema = ArticlesSchema()
+articles_chema = ArticlesSchema(many=True)
+
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,53 @@
+from crypt import methods
 from app import app,db
-from flask import jsonify
+from flask import jsonify, request
+from app.models import Articles, ArticlesSchema, article_chema , articles_chema
 
 
-@app.route('/', methods = ['GET'])
+@app.route('/get', methods = ['GET'])
 def get_articles():
-    return jsonify({'Hello': 'World'})
+    all_articles = Articles.query.all()
+    results = articles_chema.dump(all_articles)
+    return jsonify(results)
+
+
+@app.route('/get/<id>/', methods = ['GET'])
+def article_details(id):
+    article = Articles.query.get(id)
+    return article_chema.jsonify(article)
+
+
+@app.route('/add', methods= ['POST'])
+def add_article():
+    title = request.json['title']
+    body = request.json['body']
+    image_url = request.json['image_url']
+
+    articles = Articles(title, body, image_url)
+    db.session.add(articles)
+    db.session.commit()
+    return article_chema.jsonify(articles)
+
+
+@app.route('/update/<id>/', methods = ['PUT'])
+def update_article(id):
+    article = Articles.query.get(id)
+
+    title = request.json['title']
+    body = request.json['body']
+    image_url = request.json['image_url']
+
+    article.title = title
+    article.body = body
+    article.image_url = image_url
+
+    db.session.commit()
+    return article_chema.jsonify(article)
+
+
+@app.route('/delete/<id>/', methods = ['DELETE'])
+def delete_article(id):
+    article = Articles.query.get(id)
+    db.session.delete(article)
+    db.session.commit()
+    return article_chema.jsonify(article)
